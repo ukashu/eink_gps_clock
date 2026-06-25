548ce43a665494b52afab6bb0f9e09591f521842
ukashu <lukaszfilipiuk123@gmail.com>
2026-06-25 17:27:26 +0200

    changed UART interface used for debug printfs

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -61,9 +61,9 @@ int __io_putchar(int ch)
 {
     if (ch == '\n') {
         uint8_t ch2 = '\r';
-        HAL_UART_Transmit(&huart2, &ch2, 1, 1000);
+        HAL_UART_Transmit(&hlpuart1, &ch2, 1, 1000);
     }
-    HAL_UART_Transmit(&huart2, (uint8_t*)&ch, 1, 1000);
+    HAL_UART_Transmit(&hlpuart1, (uint8_t*)&ch, 1, 1000);
     return 1;
 }
 
